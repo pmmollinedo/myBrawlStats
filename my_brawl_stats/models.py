c8cb492a4b5613ac2d12d496d6311fd35cb43cc9
pmmollinedo <pmollinedo@outlook.es>
2022-10-28 17:20:57 +0200

    Models change and Mapper object deleted
    - Models now use ManyToMany Relationship.
    - The BrawlStarsApiMapper object were deleted.

--- my_brawl_stats/models.py
+++ my_brawl_stats/models.py
@@ -1,26 +1,5 @@
 from django.db import models
 
-class Player(models.Model):
-    tag = models.CharField("Player tag identificator.", max_length=20, primary_key=True)
-    name = models.CharField("Player name.", max_length=50)
-    name_color = models.CharField("Player's name color HEX code.", max_length=50)
-    icon = models.CharField("Player icon id.", max_length=50)
-    trophies = models.IntegerField("Player actual trophies.", default=0)
-    highest_trophies = models.IntegerField(default=0)
-    highest_power_play_points = models.IntegerField(default=0)
-    exp_level = models.IntegerField("Experience level.", default=0)
-    exp_points = models.IntegerField("Experience points.", default=0)
-    is_qualified_from_championship_challenge = models.BooleanField(default=False)
-    solo_victories = models.IntegerField("Singleplayer total victories.", default=0)
-    duo_victories = models.IntegerField("Duo total victories.", default=0)
-    normal_victories = models.IntegerField("3vs3 total victories.", default=0)
-    best_time_robo_rumble = models.IntegerField("Best time in Robo Rumble game mode.", default=0)
-    best_time_big_brawler = models.IntegerField("Best time in Big Brawler game mode.", default=0)
-    club_tag = models.CharField("Club tag identificator.", max_length=20)
-    club_name = models.CharField("Club name.",max_length=20)
-
-    def __str__(self):
-        return self.name
 
 class Brawler(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -28,6 +7,7 @@ class Brawler(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class StarPower(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -37,6 +17,7 @@ class StarPower(models.Model):
     def __str__(self):
         return self.name
 
+
 class Gadget(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=20)
@@ -45,51 +26,55 @@ class Gadget(models.Model):
     def __str__(self):
         return self.name
 
-class BrawlersHistory(models.Model):
-    date = models.DateTimeField()
+
+class Player(models.Model):
+    tag = models.CharField(verbose_name="Player tag identificator.",max_length=20,primary_key=True)
+    name = models.CharField(verbose_name="Player name.",max_length=50)
+    name_color = models.CharField("Name HEX code color.",max_length=50)
+    icon = models.CharField("Player icon id.", max_length=50)
+    trophies = models.IntegerField("Player actual trophies.", default=0)
+    highest_trophies = models.IntegerField(default=0)
+    highest_power_play_points = models.IntegerField(default=0)
+    exp_level = models.IntegerField("Experience level.", default=0)
+    exp_points = models.IntegerField("Experience points.", default=0)
+    is_qualified = models.BooleanField("Qualified from championship challenge", default=False)
+    solo_victories = models.IntegerField("Singleplayer total victories.", default=0)
+    duo_victories = models.IntegerField("Duo total victories.", default=0)
+    normal_victories = models.IntegerField("3vs3 total victories.", default=0)
+    best_time_robo_rumble = models.IntegerField("Best time in Robo Rumble game mode.", default=0)
+    best_time_big_brawler = models.IntegerField("Best time in Big Brawler game mode.", default=0)
+    club_tag = models.CharField("Club tag identificator.", max_length=20)
+    club_name = models.CharField("Club name.", max_length=20)
+    brawlers = models.ManyToManyField(Brawler, through='UnlockedBrawler')
+
+    def __str__(self):
+        return self.name
+
+
+class UnlockedBrawler(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     brawler = models.ForeignKey(Brawler, on_delete=models.CASCADE)
-    power = models.IntegerField(default=0)
-    rank = models.IntegerField(default=0)
-    trophies = models.IntegerField(default=0)
     highestTrophies = models.IntegerField(default=0)
+    gadgets = models.ManyToManyField(Gadget)
+    starpowers = models.ManyToManyField(StarPower)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['date', 'brawler', 'player'], name='unique_date_brawler_player_constraint'
-            )
-        ]
-    
+        unique_together = [['player', 'brawler']]
+
     def __str__(self):
         return self.brawler.name
 
-class UnlockedGadget(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    brawler = models.ForeignKey(Brawler, on_delete=models.CASCADE)
-    gadget = models.ForeignKey(Gadget, on_delete=models.CASCADE)
+
+class BrawlerStat(models.Model):
+    unlocked_brawler = models.ForeignKey(
+        UnlockedBrawler, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    power = models.IntegerField(default=0)
+    rank = models.IntegerField(default=0)
+    trophies = models.IntegerField(default=0)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['player', 'brawler', 'gadget'], name='unique_player_brawler_gadget_constraint'
-            )
-        ]
+        unique_together = [['date', 'unlocked_brawler']]
     
     def __str__(self):
-        return self.gadget.name
-
-class UnlockedStarPower(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    brawler = models.ForeignKey(Brawler, on_delete=models.CASCADE)
-    star_power = models.ForeignKey(StarPower, on_delete=models.CASCADE)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['player', 'brawler', 'star_power'], name='unique_player_brawler_starpower_constraint'
-            )
-        ]
-    
-    def __str__(self):
-        return self.star_power.name
+        return self.unlocked_brawler.brawler.name
--- my_brawl_stats/views.py
+++ my_brawl_stats/views.py
@@ -1,17 +1,98 @@
-from django.http import JsonResponse
+from datetime import datetime
 
-from .services import BrawlStarsApiClient, BrawlStarsApiException, BrawlStarsApiMapper
+from django.http import JsonResponse
+from django.utils.timezone import make_aware
+
+from .models import BrawlerStat, Player, Brawler, Gadget, StarPower, UnlockedBrawler
+
+from .services import BrawlStarsApiClient, BrawlStarsApiException
+
 
 def home(request):
     return JsonResponse(data={'message': "Welcome to the app."})
 
+
 def api(request):
+    # BRAWL STARS API
     try:
         api = BrawlStarsApiClient(player_tag="#9RO2JCJJ")
         brawlers_response = api.get_brawlers_endpoint()
         player_response = api.get_player_endpoint()
-        api_mapper = BrawlStarsApiMapper()
-        api_mapper.save_brawl_stars_data(brawlers_json=brawlers_response, player_json=player_response)
     except BrawlStarsApiException as ex:
         return JsonResponse(status= ex.error_code, data={'message': str(ex)})
+    if ("tag" in player_response and "items" in brawlers_response):
+        save_date = make_aware(datetime.now())
+
+        # PLAYER
+        player = Player(
+            tag=player_response["tag"],
+            name=player_response["name"],
+            name_color=player_response["nameColor"],
+            icon=player_response["icon"]["id"],
+            trophies=player_response["trophies"],
+            highest_trophies=player_response["highestTrophies"],
+            highest_power_play_points=player_response["highestPowerPlayPoints"],
+            exp_level=player_response["expLevel"],
+            exp_points=player_response["expPoints"],
+            is_qualified=player_response[
+                "isQualifiedFromChampionshipChallenge"],
+            solo_victories=player_response["soloVictories"],
+            duo_victories=player_response["duoVictories"],
+            normal_victories=player_response["3vs3Victories"],
+            best_time_robo_rumble=player_response["bestRoboRumbleTime"],
+            best_time_big_brawler=player_response["bestTimeAsBigBrawler"],
+            club_tag=player_response["club"]["tag"],
+            club_name=player_response["club"]["name"]
+        )
+        player.save()
+        # BRAWLERS / GADGETS / STAR POWERS
+        for json_brawler in brawlers_response["items"]:
+            brawler = Brawler(
+                id=json_brawler["id"],
+                name=json_brawler["name"]
+            )
+            brawler.save()
+            for json_gadget in json_brawler["gadgets"]:
+                gadget = Gadget(
+                    id=json_gadget["id"],
+                    name=json_gadget["name"],
+                    brawler=brawler
+                )
+                gadget.save()
+            for json_star_power in json_brawler["starPowers"]:
+                star_power = StarPower(
+                    id=json_star_power["id"],
+                    name=json_star_power["name"],
+                    brawler=brawler
+                )
+                star_power.save()
+
+        # PLAYER BRAWLERS
+        for json_unlocked_brawler in player_response["brawlers"]:
+            json_unlocked_brawler_id = json_unlocked_brawler["id"]
+            brawler: Brawler = Brawler.objects.get(pk=json_unlocked_brawler_id)
+            player.brawlers.add(brawler, through_defaults={
+                                'highestTrophies': json_unlocked_brawler["highestTrophies"]})
+            unlocked_brawler: UnlockedBrawler = UnlockedBrawler.objects.get(
+                player=player,
+                brawler=brawler
+            )
+            for json_unlocked_star_power in json_unlocked_brawler["starPowers"]:
+                unlocked_star_power = StarPower.objects.get(
+                    pk=json_unlocked_star_power["id"])
+                unlocked_brawler.starpowers.add(unlocked_star_power)
+            for json_unlocked_gadget in json_unlocked_brawler["gadgets"]:
+                unlocked_gadget = Gadget.objects.get(
+                    pk=json_unlocked_gadget["id"])
+                unlocked_brawler.gadgets.add(unlocked_gadget)
+            player.brawlers.add(brawler, through_defaults={})
+
+            brawler_stats = BrawlerStat(
+                unlocked_brawler=unlocked_brawler,
+                date=save_date,
+                power=json_unlocked_brawler["power"],
+                rank=json_unlocked_brawler["rank"],
+                trophies=json_unlocked_brawler["trophies"]
+            )
+            brawler_stats.save()
     return JsonResponse(data={'message': "Successfully updated."})
